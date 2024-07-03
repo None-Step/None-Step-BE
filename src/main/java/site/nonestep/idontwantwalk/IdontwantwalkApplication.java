9b7bd4293041b637006b16c5ea899e35e38a98ec
eunah0507 <eunah0507@naver.com>
2024-07-04 00:34:53 +0900

    :sparkles:feat: BE 초기 설정

---
+++ src/main/java/site/nonestep/idontwantwalk/IdontwantwalkApplication.java
@@ -0,0 +1,13 @@
+package site.nonestep.idontwantwalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdontwantwalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdontwantwalkApplication.class, args);
+	}
+
+}
