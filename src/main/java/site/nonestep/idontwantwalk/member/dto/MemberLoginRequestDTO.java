5c9498fd33abe8f8a2dfe45d39a58209cc351244
heyyeon <clsgks92@naver.com>
2024-07-31 13:06:04 +0900

    :sparkles:feat: 일반로그인 기능 구현

---
+++ src/main/java/site/nonestep/idontwantwalk/member/dto/MemberLoginRequestDTO.java
@@ -0,0 +1,12 @@
+package site.nonestep.idontwantwalk.member.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class MemberLoginRequestDTO {
+    private String memberID;
+    private String memberPass;
+
+}
--- src/main/java/site/nonestep/idontwantwalk/member/service/MemberService.java
+++ src/main/java/site/nonestep/idontwantwalk/member/service/MemberService.java
@@ -13,5 +13,11 @@ public interface MemberService {
 
     //id 찾기
     List<MemberIdFindResponseDTO> idFind (MemberIdFindRequestDTO memberIdFindRequestDTO); //여러개의 정보가 나와서 list로 가져옴
+
+    //일반로그인
+    Long login(String memberID, String memberPass); //우선 자료타입과 변수명을 적은 후 어떤 값을 받아오는지 생각해보기(
+    //일반로그인 시 DB에 Refresh Token을 저장
+    void refreshlogin (Long memberNo, String memberRefreshToken);
+
 }
 
